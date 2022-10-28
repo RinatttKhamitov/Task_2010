a7e1aebedb97527d5b78e5d3aa19630a49207ed9
Ринат Хамитов <stoneaz@mail.ru>
2022-10-28 17:57:16 +0300

    дз

---
+++ Тумаков/Program.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тумаков
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { };
+            Bank bank = new Bank();
+            bank.Print();
+            Console.WriteLine();
+            Console.WriteLine("2");
+            Bank2 bank2 = new Bank2();
+            SetAccountNumber(ref list, ref bank2);
+            bank2.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("3");
+            Bank3 bank3 = new Bank3();
+            bank3.Deposit(10000);
+            bank3.Withdraw(100);
+            bank3.Print();
+            Console.ReadKey();
+
+        }
+        static void SetAccountNumber(ref List<int> list, ref Bank2 bank)
+        {
+            Random rnd = new Random();
+
+            //Получить случайное число (в диапазоне от 0 до 10)
+            int value = rnd.Next(1000000, 10000000);
+            while (list.Contains(value))
+            {
+                value = rnd.Next(1000000, 10000000);
+            }
+            bank.AccountNumber = value; 
+            list.Add(value);
+        }
+    }
+    class Bank
+    {
+
+        protected int accountNumber = 1234567342;
+        protected string password = "123321";
+        enum BankType { current, savings }
+        private BankType BankAccountType = BankType.current;
+        public int AccountNumber
+        {
+            get
+            {
+                Console.Write("Введи пароль: ");
+                if (Console.ReadLine() == password)
+                {
+                    return accountNumber;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        protected int balance = 10000;
+
+        public int Balance
+        {
+            get
+            {
+                Console.Write("Введи пароль: ");
+                if (Console.ReadLine() == password)
+                {
+                    return balance;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                    return 0;
+                }
+            }
+
+        }
+        public void Print()
+        {
+            Console.Write("Введи пароль: ");
+            if (Console.ReadLine() == password)
+            {
+                Console.Write($"номер счета {accountNumber} \nбаланс {balance} \nтип банковского счета {BankAccountType}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+
+
+
+    class Bank2
+    {
+
+        protected int accountNumber = 1234567342;
+        protected string password = "123321";
+        enum BankType { current, savings }
+        private BankType BankAccountType = BankType.current;
+        public int AccountNumber
+        {
+            get
+            {
+                Console.Write("Введи пароль: ");
+                if (Console.ReadLine() == password)
+                {
+                    return accountNumber;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                accountNumber = value;
+            }
+        }
+
+        protected int balance = 10000;
+
+        public int Balance
+        {
+            get
+            {
+                Console.Write("Введи пароль: ");
+                if (Console.ReadLine() == password)
+                {
+                    return balance;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                    return 0;
+                }
+            }
+
+        }
+        public void Print()
+        {
+            Console.Write("Введи пароль: ");
+            if (Console.ReadLine() == password)
+            {
+                Console.Write($"номер счета {accountNumber} \nбаланс {balance} \nтип банковского счета {BankAccountType}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+    }
+    class Bank3
+    {
+
+        protected int accountNumber = 1234567342;
+        protected string password = "123321";
+        enum BankType { current, savings }
+        private BankType BankAccountType = BankType.current;
+        public int AccountNumber
+        {
+            get
+            {
+                Console.Write("Введи пароль: ");
+                if (Console.ReadLine() == password)
+                {
+                    return accountNumber;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                accountNumber = value;
+            }
+        }
+
+        protected int balance;
+        public int Balance
+        {
+            get
+            {
+                Console.Write("Введи пароль: ");
+                if (Console.ReadLine() == password)
+                {
+                    return balance;
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                    return 0;
+                }
+            }
+
+        }
+        public void Print()
+        {
+            Console.Write("Введи пароль: ");
+            if (Console.ReadLine() == password)
+            {
+                Console.Write($"номер счета {accountNumber} \nбаланс {balance} \nтип банковского счета {BankAccountType}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+        }
+        public void Withdraw(int money)
+        {
+            if (balance >= money)
+            {
+                balance -= money;
+            }
+            else
+            {
+                Console.WriteLine("Нету денег на счете");
+            }
+        }
+        public void Deposit(int money)
+        {
+            balance += money;
+        }
+    }
+    class Home
+    {
+        protected int buildingNumber;
+        public int AccountNumber
+        {
+            get
+            {
+                return buildingNumber;
+            }
+            set
+            {
+                buildingNumber = value;
+            }
+        }
+
+    }
+
+}
